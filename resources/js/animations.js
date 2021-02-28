688250dad8a5843e2f9aaf3368731a020d2bd258
Yoannou <yoannou300@gmail.com>
2021-02-28 14:48:19 -0500

    Added rotating hologram image functionality. Compressed hologram images.

--- resources/js/animations.js
+++ resources/js/animations.js
@@ -4,7 +4,14 @@ const nameWrapper = document.querySelector(".name-wrapper");
 const nameArray = ["", "J", "A", "M", "E", "S", "", "Y", "O", "A", "N", "N", "O", "U"];
 const webDeveloper = document.querySelector(".web-developer");
 
-/********** MAIN PROGRAM **********/
+const hologram = document.getElementById("hologram-image");
+const hologramReference = document.getElementById("hologram-reference");
+const aboutHeading = document.querySelector(".about-heading");
+let viewportMarkers = new Array(4);
+createViewportMarkers(viewportMarkers);
+
+
+/********** ADDING MY NAME **********/
 
 /* Add my name to the grid with 0 opacity */
 for(let i = 0; i < nameArray.length; i++) {
@@ -21,7 +28,32 @@ for(let i = 0; i < nameArray.length; i++) {
 
 /* Add a letter every 10th of a second */
 let count = 0;
-let timer = setInterval(addLetter, 100);
+let timer = setInterval(addLetter, 85);
+
+
+/********** ROTATING FACE **********/
+window.addEventListener('resize', () => { createViewportMarkers(viewportMarkers) });
+
+document.addEventListener('scroll', () => {
+    if (isInViewport(hologram)){
+        let hologramPos = hologram.getBoundingClientRect();
+        if (hologramPos.top > viewportMarkers[0]) {
+            hologram.src = "resources/img/compressedpng/holo1(90 deg)-min.png";
+        }
+        else if (hologramPos.top > viewportMarkers[1]){
+            hologram.src = "resources/img/compressedpng/holo2(45 deg)-min.png";
+        }
+        else if (hologramPos.bottom < viewportMarkers[3]){
+            hologram.src = "resources/img/compressedpng/holo5(-90 deg)-min.png";
+        }
+        else if (hologramPos.bottom < viewportMarkers[2]){
+            hologram.src = "resources/img/compressedpng/holo4(-45 deg)-min.png";
+        }
+        else {
+            hologram.src = "resources/img/compressedpng/holo3(0 deg)-min.png";
+        }
+    }
+});
 
 
 /********** FUNCTIONS **********/
@@ -46,4 +78,25 @@ function addLetter(){
         count++;
     }
 
+}
+
+/* Checks if element is in the viewport */
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top < (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.bottom > 0
+    );
+}
+
+/* Returns an array of 4 markers representing fractions of the viewports height */
+function createViewportMarkers(arr) {
+    console.log("Creating new viewport markers...");
+    for(let i = 0; i < 4; i ++) {
+        arr.pop();
+    }
+    arr.push(window.innerHeight - window.innerHeight / 4);
+    arr.push(window.innerHeight - window.innerHeight / 3);
+    arr.push(window.innerHeight / 2);
+    arr.push(window.innerHeight / 3);
 }
